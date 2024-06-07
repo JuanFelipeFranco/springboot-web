2327cc68b737968df91d981e1e8891ad6454580b
Juan Felipe Franco <juanfelipefrancorosas@gmail.com>
2024-06-06 21:04:26 -0500

    Crate HomeController - Redirect
    En este caso creamos nuestro nuevo controller HOME, donde si tenemos nuestro url
    localhost:8090 o localhost:8090/ o localhost:8090/home nos va redirigir a la direccion localhost:8090/list

---
+++ springboot-web/src/main/java/com/felipe/spring_webapp/springboot_web/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.felipe.spring_webapp.springboot_web.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping({"","/","home"})
+    public String home(){
+
+        return "redirect:/list"; //ruta que redirecciona al listado, ya que muestra el nuevo usuario agregado
+    }
+}
